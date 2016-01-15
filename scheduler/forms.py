57a4ffbdd66d96521d6eb8348d016339f4dc4f12
Thejaswi Puthraya <thejaswi.puthraya@gmail.com>
2016-01-15 12:27:11 +0530

    Removed the usage of subclassed SplitDateTimeField.

--- scheduler/forms.py
+++ scheduler/forms.py
@@ -1,8 +1,6 @@
-import datetime
 import pytz
 
 from django import forms
-from django.forms.utils import from_current_timezone
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from django.core.exceptions import ValidationError
@@ -12,44 +10,18 @@ from allauth.socialaccount.models import SocialAccount
 from .models import User, ScheduledPost
 
 
-class SplitDateTimeField(forms.SplitDateTimeField):
-    def __init__(self, *args, **kwargs):
-        super(SplitDateTimeField, self).__init__(*args, **kwargs)
-        self.default_error_messages['invalid_tz'] = _('Enter a valid tz')
-        self.fields = self.fields + (
-            # TZ field
-            forms.CharField(),
-        )
-
-    def compress(self, data_list):
-        if data_list:
-            # Raise a validation error if time or date is empty
-            # (possible if SplitDateTimeField has required=False).
-            if data_list[0] in self.empty_values:
-                raise ValidationError(self.error_messages['invalid_date'], code='invalid_date')
-            if data_list[1] in self.empty_values:
-                raise ValidationError(self.error_messages['invalid_time'], code='invalid_time')
-            if data_list[2] in self.empty_values:
-                raise ValidationError(self.error_messages['invalid_tz'], code='invalid_tz')
-            result = datetime.datetime.combine(data_list[0], data_list[1])
-            timezone.activate(pytz.timezone(data_list[2]))
-            tz_aware_datetime = from_current_timezone(result)
-            timezone.deactivate()
-            return tz_aware_datetime
-        return None
-
 class SplitDateTimeWidget(forms.SplitDateTimeWidget):
     def __init__(self, *args, **kwargs):
         super(SplitDateTimeWidget, self).__init__(*args, **kwargs)
         self.widgets[0].input_type = "date"
         self.widgets[1].input_type = "time"
-        self.widgets.append(forms.HiddenInput())
 
 
 class ScheduledPostAddForm(forms.ModelForm):
-    scheduled_datetime = SplitDateTimeField(
+    scheduled_datetime = forms.SplitDateTimeField(
         widget=SplitDateTimeWidget
     )
+    scheduled_tz = forms.CharField(widget=forms.HiddenInput)
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop("user")
@@ -75,3 +47,12 @@ class ScheduledPostAddForm(forms.ModelForm):
         if scheduled_datetime < timezone.now():
             raise forms.ValidationError("Time cannot be in the past")
         return scheduled_datetime
+
+    def clean(self):
+        data = self.cleaned_data
+        if data.get("scheduled_datetime"):
+            sched_dt = data["scheduled_datetime"]
+            tz = pytz.timezone(data.get("scheduled_tz", "UTC"))
+            sched_dt = tz.localize(sched_dt.replace(tzinfo=None))
+            data["scheduled_datetime"] = timezone.localtime(sched_dt)
+        return data
